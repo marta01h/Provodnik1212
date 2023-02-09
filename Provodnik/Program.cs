53c004945f6b892a509ced0a3649046c98ed442b
marta01h <113929646+marta01h@users.noreply.github.com>
2023-02-09 12:24:35 +0300

    Update Program.cs

--- Provodnik/Program.cs
+++ Provodnik/Program.cs
@@ -1,99 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _7practic
+namespace Practical6
 {
-    internal class Options
+    internal class Program
     {
-        ConsoleKey Key_Pressed;
-        public string path;
-        public string path1;
-        public string File_Name;
-        public string Directory_Name;
-        public void Coise()
+        static void Main(string[] args)
         {
-
+            Console.WriteLine("Введите путь до файла (вместе с названием), который вы хотите открыть");
+            Console.WriteLine("---------------------------------------------------------------------");
+            List<Figure> figures = ReadSave.Read(Console.ReadLine());
+            figures = TextChange.Change(figures);
             Console.Clear();
-            Console.CursorVisible = true;
-            Console.WriteLine("Выход из меню вариаций - Esc\n ");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("  Новый файл - F2");
-            Console.WriteLine("  Удаление файла - F3");
-            Console.WriteLine("  Новая директория - F4");
-            Console.WriteLine("  Удаление директории - F5");
-            Console.ResetColor();
-            while (true)
-            {
-                ConsoleKeyInfo key = Console.ReadKey();
-                if (key.Key == ConsoleKey.F2)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Путь создания файла: ");
-                    path = Console.ReadLine();
-                    Console.WriteLine("Наименование файл ");
-                    File_Name = Console.ReadLine();
+            Console.WriteLine("Введите путь до файла (вместе с названием), куда вы хотите сохранить текст");
+            Console.WriteLine("---------------------------------------------------------------------");
+            ReadSave.Save(Console.ReadLine(), figures);
 
-                    File.Create(path + @"\" + File_Name);
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Отлично! Файл создан");
-                    Thread.Sleep(1500);
-                    Console.ResetColor();
-                    break;
-                }
 
-                else if (key.Key == ConsoleKey.F3)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Пусть для удаления");
-                    path = Console.ReadLine();
-
-                    File.Delete(path);
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Удаление файла успешно");
-                    Thread.Sleep(1500);
-                    Console.ResetColor();
-                    break;
-                }
-
-                else if (key.Key == ConsoleKey.F4)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Укажите путь для создания папки: ");
-                    path = Console.ReadLine();
-                    Console.WriteLine("Укажите имя: ");
-                    Directory_Name = Console.ReadLine();
-
-                    Directory.CreateDirectory(path + @"\" + Directory_Name);
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Папка создана");
-                    Thread.Sleep(1500);
-                    Console.ResetColor();
-                    break;
-                }
-
-                else if (key.Key == ConsoleKey.F5)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Укажите путь для удаления папки: ");
-                    path = Console.ReadLine();
-
-                    Directory.Delete(path, true);
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Папка удалена");
-                    Thread.Sleep(1500);
-                    Console.ResetColor();
-                    break;
-
-                }
-
-                else if (key.Key == ConsoleKey.Escape)
-                {
-                    break;
-                }
-            }
         }
     }
 }
